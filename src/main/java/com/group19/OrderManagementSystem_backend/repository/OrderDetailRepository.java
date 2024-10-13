710955fc04d056964da9a0d03f9e2b7def727f0f
Tuananh1523 <tanh15203@gmail.com>
2024-10-14 01:17:45 +0700

    Create OrderDetailRepository.java
    OrderDetailRepository

---
+++ src/main/java/com/group19/OrderManagementSystem_backend/repository/OrderDetailRepository.java
@@ -0,0 +1,16 @@
+package com.group19.OrderManagementSystem_backend.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.group19.OrderManagementSystem_backend.entity.Order;
+import com.group19.OrderManagementSystem_backend.entity.Order_detail;
+import com.group19.OrderManagementSystem_backend.entity.Order_detail_ID;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<Order_detail, Order_detail_ID> {
+	List<Order_detail> findByOrder(String order_id);
+}
