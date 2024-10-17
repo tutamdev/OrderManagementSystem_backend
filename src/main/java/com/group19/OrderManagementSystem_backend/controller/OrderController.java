1709829d6f0b6f47efa66bdea5a6425471f16b2c
Tuananh1523 <tanh15203@gmail.com>
2024-10-17 16:38:14 +0700

    OrderController

---
+++ src/main/java/com/group19/OrderManagementSystem_backend/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.group19.OrderManagementSystem_backend.controller;
+
+import com.group19.OrderManagementSystem_backend.entity.Order;
+import com.group19.OrderManagementSystem_backend.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable String id) {
+        return orderService.getOrderById(id)
+                .map(order -> ResponseEntity.ok(order))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable String id) {
+        orderService.deleteOrder(id);
+        return ResponseEntity.noContent().build();
+    }
+}
