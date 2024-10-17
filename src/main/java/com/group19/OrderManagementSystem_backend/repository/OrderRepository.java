32a8823a0f240082af877888a101ecc3abf8b10c
Tuananh1523 <tanh15203@gmail.com>
2024-10-17 16:38:41 +0700

    Update OrderRepository.java

--- src/main/java/com/group19/OrderManagementSystem_backend/repository/OrderRepository.java
+++ src/main/java/com/group19/OrderManagementSystem_backend/repository/OrderRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
 	
-	List<Order> findByCreatedBy(String id);//tìm theo người tạo
+	List<Order> findByEmployee_Id(String id);//tìm theo người tạo
 	
 }
